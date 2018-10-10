7e237f21d35469d5a7b097662e56de3a5a1d1bba
Matheus <ferreira.cesar2@gmail.com>
2018-10-10 15:23:28 -0300

    modificando pg humor

--- Codigo/paginahumor.php
+++ Codigo/paginahumor.php
@@ -57,64 +57,86 @@ body {
 #titulo_cabeçalho {
   font-family: Freestyle Script;
   font-size: 40px;
-  margin: 6px;
+  margin-top: 1px;
 }
 
-#cabeçalho {
-  background-color: #ffeb3b;
-  position: fixed;
-  width: 1580px;
-  height: 60px;
-  margin: 6px;
-}
+
 a:-webkit-any-link {
     color: black;
     cursor: pointer;
     text-decoration: none;
 }
 
+
 /*botão do menu fixo*/
 
+
+
+
 .dropbtn {
-	    background-color :#ebec8f;
-		color : black;
-		padding: 15px;
-		font-size: 20px;
-		border: 200px;
-     }
-	 .dropbtn {
-       position: fixed;
-	   display: inline;
-	   width: 200px;
+	 background-color :#ffeb3b;
+	 color : black;
+	 padding: 20px;
+	 font-size: 20px;
+	 border: px;
+	 margin-left: 200px;
 
-	 }
-	 .dropdown-content {
-	   display: none;
-	   position: absolute;
-	   background-color: lightgrey;
-	   min-width: 150px;
-	   z-index: 1;
 
-	 }
-     .dropdown-content a {
-       color: black;
-	   padding: 12px 16px;
-	   text-decoration: none;
-	   display: block;
-	   width: 150px;
-	 }
+		}
 
-	 .dropdown-content a:hover {
-     background-color: #ebec8f;
-   }
-	 .dropdown:hover .dropdown-content {
-     display:block;
-   }
-	 .dropdown:hover .dropbtn {
-     background-color: gray;
-   }
+	.dropbtn {
+			position: fixed;
+		display: inline;
+		width: 2000px;
+
+	}
+
+
+	.dropdown-content {
+		display: none;
+		position: absolute;
+		background-color: lightgrey;
+		min-width: 150px;
+		z-index: 1;
+
+	}
+
+
+		.dropdown-content a {
+			color: black;
+		padding: 12px 16px;
+		text-decoration: none;
+		display: inline-flex;
+		width: 2000px;
+
+	}
+
+	.dropdown-content a:hover {background-color: #ebec8f;}
+	.dropdown:hover   .dropdown-content {display:block;}
+	.dropdown:hover   .dropbtn {background-color: gray;}
 
 </style>
+</head>
+
+
+<div class= "dropdown">
+<button class="dropbtn">all for onde</button>
+<div class="dropdown-content">
+
+
+<a href="https://mail.google.com">HUMOR</a>
+<a href="https://github.com/">DIÁRIO</a>
+<a href="https://globoesporte.globo.com">HOME</a>
+
+
+</div>
+</div>
+
+</style>
+</head>
+
+
+
 <head>
   <title>Página Inicial</title>
   <meta charset = "utf-8">
@@ -122,32 +144,15 @@ a:-webkit-any-link {
 
 <body>
   <div id="cabeçalho">
-    <p id="titulo_cabeçalho">all for one</p>
-
-    <div class= "dropdown">
-      <button class="dropbtn">página</button>
-      <div class="dropdown-content">
-
-
-        <a href="humor.html">HUMOR</a>
-        <a href="humor.html">DIÁRIO</a>
-        <a href="humor.html">HOME</a>
-
-  </div>
+    <p id="titulo_cabeçalho"></p>
   <div id="corpo">
 
-  <div id="divtítulo"> <h1 id='Título'> all for one </h1>
+  <div id="divtítulo"> <h1 id='Título'> all for onde</h1>
   <div id= "divmenu">
-	<ul>
-	<li><a href='paginahome.php'>HOME</a></li> |
-	<li><a href='paginadiario.html'>DIÁRIO</a></li> |
-	<li><a href='paginahumor.html'>HUMOR</a></li>|
-	</ul>
-  </div>
+
+</div>
 </div>
  <br>
- <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>a
- <br><br><br><br><br><br><br><br><br><br><br><br><br><br>
-</div>
+ <br><br><br><br>
 </body>
 </html>
